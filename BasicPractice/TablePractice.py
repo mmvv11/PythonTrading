48b6fadeb81f2826eb0cc8ad7a08cc41d686e587
ronnie-colmen <ronnie@softsquared.com>
2020-11-12 23:37:38 +0900

    테이블 예제 - 주석 추가

--- BasicPractice/TablePractice.py
+++ BasicPractice/TablePractice.py
@@ -13,6 +13,7 @@ class TablePractice(QMainWindow, form):
         super(TablePractice, self).__init__()
         # ui 파일을 적용합니다.
         self.setupUi(self)
+        # 테이블 행 사이즈를 지정합니다.
         self.tb_main.setRowCount(len(tickers))
 
         # 타이머를 적용합니다.
@@ -22,8 +23,11 @@ class TablePractice(QMainWindow, form):
 
     # 테이블 행에 넣을 마켓 데이터를 가져오는 함수
     def get_marget_info(self, ticker):
+        # 빗썸 서버에서 가상화폐에 대한 데이터를 가져옵니다.
         df = pybithumb.get_candlestick(ticker)
+        # 종가에 대해 5일 간격의 이동평균을 구합니다.
         mean_average = df['close'].rolling(5).mean()
+        # 전날의 이동평균값을 가져옵니다.
         last_mean_average = mean_average[-2]
         price = pybithumb.get_current_price(ticker)
 
@@ -41,6 +45,7 @@ class TablePractice(QMainWindow, form):
             self.tb_main.setItem(i, 1, QTableWidgetItem(str(price)))
             self.tb_main.setItem(i, 2, QTableWidgetItem(str(last_average)))
             self.tb_main.setItem(i, 3, QTableWidgetItem(state))
+
 
 app = QApplication(sys.argv)
 window = TablePractice()
